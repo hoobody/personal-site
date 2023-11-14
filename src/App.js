87b664da11f5d0784dc56ab8f38f7b651e86b6e6
hoobody <jacob@charbo.net>
2023-11-14 14:00:08 -0800

    Changed text on main screen

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React Website</h1>
+        <h1>Welcome to Jacob's Cool Website</h1>
       </header>
       <main>
         <p>This is a simple React website.</p>
